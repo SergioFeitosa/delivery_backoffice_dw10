9b38b47fdd8ea2b05b38487b716095279be3930a
Sergio Feitosa <sergiofeitosa@gmail.com>
2023-05-24 11:01:05 -0300

    Primeiro commit

---
+++ lib/src/core/storage/session_storage_impl.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+import './session_storage.dart';
+
+class SessionStorageImpl extends SessionStorage {
+
+  @override
+  String getData(String key) =>
+    window.sessionStorage[key] ?? '';
+  
+  @override
+  void setData(String key, String value) =>
+    window.sessionStorage[key] = value;
+  
+  @override
+  void clean() {
+    window.sessionStorage.clear();
+  }
+
+}
